b89e1583dee70db5df9a8b8a7a25c732d81f121c
hatuw <jiaxi_wu@ieee.org>
2018-09-02 20:02:11 +0800

    update

--- code/protocol.h
+++ code/protocol.h
@@ -2526,22 +2526,21 @@ bool gwas_protocol(MPCEnv& mpc, int pid) {
 bool test_protocol(MPCEnv& mpc, int pid) {
   // return gwas_protocol(mpc, pid);
 
-  int vec_len = 5;
+  int vec_len = 3;
   Vec<ZZ_p> test_vec, test_vec2;
 
   test_vec.SetLength(vec_len);
   test_vec2.SetLength(vec_len);
   for (int i = 0; i < vec_len; i++) {
     test_vec[i] = 0;
-    test_vec2[i] = 1;
+    test_vec2[i] = 0;
   }
-  // mpc.RandVec(test_vec, 5);
-  // mpc.RandVec(test_vec2, vec_len);
+  test_vec2[2] = 2;
 
-  // Mat<ZZ> test_mat;
-  // mpc.RandMat(test_mat, 10, 10, 20);
-
-  mpc.Householder(test_vec, test_vec2);
+  if (pid == 1) {
+    mpc.Householder(test_vec, test_vec2);
+  }
+  // mpc.Powers(test_vec2, test_vec, 5)
   return true;
 }
 
